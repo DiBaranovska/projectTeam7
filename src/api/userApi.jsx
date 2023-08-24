cb271d6aff42f5fc22c4af6491853d063495891a
Diana Baranovska <dmitrudi@gmail.com>
2023-08-24 20:42:50 +0300

    додає токен у заголовки, компонент btn

--- src/api/userApi.jsx
+++ src/api/userApi.jsx
@@ -1,1 +1,9 @@
+import axios from 'axios';
 
+export const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+/*const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};*/
