a7001b30580b332b0482da55f620abd0392394ab
Diana Baranovska <dmitrudi@gmail.com>
2023-08-29 00:10:13 +0300

    додає акценти до bg, та фавікон

--- src/components/sharedLayout/sharedLayout.jsx
+++ src/components/sharedLayout/sharedLayout.jsx
@@ -3,14 +3,19 @@ import { Outlet } from 'react-router-dom';
 import * as React from 'react';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import css from './sharedLayout.module.scss';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
-      <Suspense>
-        <Outlet />
-      </Suspense>
+      <main>
+        <div className={css.block_bg_radial}></div>
+        <div className={css.block_bg_radial_2}></div>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </main>
       <Footer />
     </>
   );
