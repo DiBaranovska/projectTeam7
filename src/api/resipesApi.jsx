bc4db2bc50dd508e3f7b8f35eab96927ef055e6e
Vladyslav Khmilevskyi <khmiluvkiyv.ku@gmail.com>
2023-08-28 20:28:20 +0300

    Adding and removing from favorites

--- src/api/resipesApi.jsx
+++ src/api/resipesApi.jsx
@@ -1,6 +1,7 @@
-const getRecipeData = async (recipeId) => {
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZWJhNzY1MjZhYTJlNDBmYWU1ZjUxZCIsImlhdCI6MTY5MzIzNTA3NCwiZXhwIjoxNjkzMzE3ODc0fQ.A5meq6tMr1SzWTte8dUPS-d3YBHVmVAuHvOc9DJtF1M';
+
+export const getRecipeData = async (recipeId) => {
   const API_URL = `https://projectteam7-backend.onrender.com/recipes/${recipeId}`;
-  const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZTlhNjI1MzJkYTU4ODc4NWI5NDI1MSIsImlhdCI6MTY5MzAzNDAyMSwiZXhwIjoxNjkzMTE2ODIxfQ.YK41zRMmSN06PNyQFuddAvVzNL2iE0ahySvqEODTNM8'; // Ваш токен
 
   try {
     const response = await fetch(API_URL, {
@@ -17,4 +18,62 @@ const getRecipeData = async (recipeId) => {
   }
 };
 
-export default getRecipeData;
+export const addToFavorites = async (recipeId) => {
+  const API_URL = 'https://projectteam7-backend.onrender.com/favorite';
+
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'accept': 'application/json',
+      'Authorization': `Bearer ${TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id: recipeId })
+  });
+
+  if (response.ok) {
+    console.log('Recipe added to favorites');
+  } else {
+    console.error('Failed to add recipe to favorites');
+  }
+};
+
+export const removeFromFavorites = async (recipeId) => {
+  const API_URL = `https://projectteam7-backend.onrender.com/favorite/${recipeId}`;
+
+  const response = await fetch(API_URL, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${TOKEN}`
+    }
+  });
+
+  if (response.ok) {
+    console.log('Recipe removed from favorites');
+  } else {
+    console.error('Failed to remove recipe from favorites');
+  }
+};
+
+export const checkFavorite = async (recipeId) => {
+  const API_URL = `https://projectteam7-backend.onrender.com/favorite?page=1`;
+
+  const response = await fetch(API_URL, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${TOKEN}`
+    }
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    const favoriteRecipes = data.recipes;
+
+    const isFavorite = favoriteRecipes.some(recipe => recipe._id === recipeId);
+    // console.log(isFavorite)
+    return isFavorite;
+  } else {
+    console.error('Failed to check if recipe is favorite');
+    throw new Error('Failed to check if recipe is favorite');
+  }
+};
--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import getRecipeData from '../../api/resipesApi.jsx';
-import css from './RecipePage.module.scss'
+import {getRecipeData} from '../../api/resipesApi.jsx';
+import css from './RecipePage.module.scss';
 
 import RecipeIngredientsList from '../../components/recipeInngredientsList/recipeInngredientsList.jsx';
 import Preparation from '../../components/recipePreparation/recipePreparation.jsx';
 import Field from '../../components/recipeDescriptionFields/recipeDescriptionFields.jsx';
 
 const RecipePage = () => {
+  const [recipeId, setRecipeId] = useState("");
   const [recipeData, setRecipeData] = useState({
     category: "",
     glass: "",
@@ -16,7 +17,7 @@ const RecipePage = () => {
   });
 
   useEffect(() => {
-    const recipeId = '639b6de9ff77d221f190c56a';
+    const recipeId = '639b6de9ff77d221f190c62e';
     getRecipeData(recipeId)
       .then(data => {
         if (data) {
@@ -29,14 +30,18 @@ const RecipePage = () => {
             instructions: data.instructions
           });
         }
+      setRecipeId(recipeId);
+      })
+      .catch(error => {
+        console.error('An error occurred:', error);
       });
   }, []);
 
   const { category, glass, drinkThumb, ingredients, instructions } = recipeData;
 
   return (
-    <div className={css.recipe__conteiner}>
-      <Field glass={glass} category={category} drinkThumb={drinkThumb}/>
+    <div className={css.conteiner__recipe}>
+      <Field glass={glass} category={category} drinkThumb={drinkThumb} recipeId={recipeId} />
 
       <RecipeIngredientsList ingredients={ingredients} />
 
