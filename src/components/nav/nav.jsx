21dae1b141fc6d1be7b50666a80531e1c9bfc52f
Diana Baranovska <dmitrudi@gmail.com>
2023-09-04 03:06:08 +0300

    fix-FooterLink

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import styles from './nav.module.scss';
 export const Nav = () => {
-  return <nav className={styles.navigate}>
-  <Link className={styles.navLink} to="/drinks/:categoryName">Drinks</Link>
-  <Link className={styles.navLink} to="/add">Add recipes</Link>
-  <Link className={styles.navLink} to="/my">My recipes</Link>
-  <Link className={styles.navLink} to="/favorite">Favorites</Link>
-</nav>
+  return (
+    <nav className={styles.navigate}>
+      <Link className={styles.navLink} to="/drinks">
+        Drinks
+      </Link>
+      <Link className={styles.navLink} to="/add">
+        Add recipes
+      </Link>
+      <Link className={styles.navLink} to="/my">
+        My recipes
+      </Link>
+      <Link className={styles.navLink} to="/favorite">
+        Favorites
+      </Link>
+    </nav>
+  );
 };
-
-
-
