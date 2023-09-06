f237995f7a4a74d6a905078182cb7cf9d7c27968
Diana Baranovska <dmitrudi@gmail.com>
2023-09-07 00:32:10 +0300

    fix-drinksPage

--- src/components/drinksSearch/drinksForm.jsx
+++ src/components/drinksSearch/drinksForm.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
 import {
   setFilterCriteria,
   clearFilterCriteria,
